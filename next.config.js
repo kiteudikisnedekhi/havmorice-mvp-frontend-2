fbb06fbb4d3f484405f75667285d52347d1e06ef
kiteudikisnedekhi-bb <jsbca202@gmail.com>
2025-10-27 02:14:41 +0530

    let us see

--- next.config.js
+++ next.config.js
@@ -2,7 +2,15 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
 });
-
 module.exports = withPWA({
-  // Your config
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
 });
